693059c4d61648d26e51508c0a47e7c59c6c4c17
vivekbishwokarma99 <risesbudhathoki5@gmail.com>
2021-06-14 00:02:36 +0545

    input length breadth height & print area of right angled triangle

---
+++ LabOne/Qn_Two.py
@@ -0,0 +1,11 @@
+'''
+Write a program that reads the length, base and height of a right-angled triangle
+and print the are. Every number is give non separate line.
+'''
+l = int(input("enter the length of a right-angled triangle:"))
+b = int(input("enter the base of a right-angled triangle:"))
+h = int(input("enter the height of a right-angled triangle:"))
+area = l * b * h
+print(f"The area of right-angled triangle is: {area}")
+
+
