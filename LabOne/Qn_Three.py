e3f7027c43432b3b55c90269b69ddf0daa8daf79
vivekbishwokarma99 <risesbudhathoki5@gmail.com>
2021-06-14 00:50:56 +0545

    distribution of apples to students

--- LabOne/Qn_Three.py
+++ LabOne/Qn_Three.py
@@ -3,3 +3,9 @@
    part remains in the basket. How many apples will each single student get? How many apples will remain in the basket?
    The program reads the number N and K. It should print the two answers for the questions above.
 '''
+N = int(input("Enter the total number of students:"))
+K = int(input("Enter the total number of apples in a basket"))
+app = K // N
+rem = K % N
+print(f"Number of apples each student will get: {app}")
+print(f"Number of remaining apples in a basket: {rem} ")
