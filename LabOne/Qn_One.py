f6fe6a6be67c8e60ec14c4d3262265f5f8c13dbf
vivekbishwokarma99 <risesbudhathoki5@gmail.com>
2021-06-13 23:45:00 +0545

    initial commit

---
+++ LabOne/Qn_One.py
@@ -0,0 +1,8 @@
+#1.Write a program that takes three numbers and prints their sum.
+a = int(input("enter first number:"))
+b = int(input("enter second number"))
+c = int(input("enter third number"))
+sum = a + b + c
+print(f"the sum of three numbers is: {sum}")
+
+
