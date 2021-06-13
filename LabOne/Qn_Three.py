c14eed6dee8c5e0d675cbd30d0db011da5203484
vivekbishwokarma99 <risesbudhathoki5@gmail.com>
2021-06-14 00:24:38 +0545

    fixing area

---
+++ LabOne/Qn_Three.py
@@ -0,0 +1,5 @@
+'''
+3. N students take K apples and distribute them among each other evenly. The remaining (the undivisible)
+   part remains in the basket. How many apples will each single student get? How many apples will remain in the basket?
+   The program reads the number N and K. It should print the two answers for the questions above.
+'''
--- LabOne/Qn_Two.py
+++ LabOne/Qn_Two.py
@@ -5,7 +5,7 @@ and print the are. Every number is give non separate line.
 l = int(input("enter the length of a right-angled triangle:"))
 b = int(input("enter the base of a right-angled triangle:"))
 h = int(input("enter the height of a right-angled triangle:"))
-area = l * b * h
+area = (b * h) / 2
 print(f"The area of right-angled triangle is: {area}")
 
 
