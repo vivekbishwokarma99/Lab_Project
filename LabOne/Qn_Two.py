95fb282fbbee88327eb56bcd205ef6e8325237d3
vivekbishwokarma99 <risesbudhathoki5@gmail.com>
2021-06-14 00:31:49 +0545

    finding area of right angled triangle

--- LabOne/Qn_Two.py
+++ LabOne/Qn_Two.py
@@ -1,8 +1,7 @@
 '''
-Write a program that reads the length, base and height of a right-angled triangle
+Write a program that reads the length of base and height of a right-angled triangle
 and print the are. Every number is give non separate line.
 '''
-l = int(input("enter the length of a right-angled triangle:"))
 b = int(input("enter the base of a right-angled triangle:"))
 h = int(input("enter the height of a right-angled triangle:"))
 area = (b * h) / 2
